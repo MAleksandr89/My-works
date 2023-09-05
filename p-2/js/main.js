a222fdd76dfd3a35ccf6d39ca68dfd80393bb047
MAleksandr89 <107121731+MAleksandr89@users.noreply.github.com>
2023-09-05 13:52:31 +0300

    Добавил новый проект

---
+++ p-2/js/main.js
@@ -0,0 +1,6 @@
+const howItsWork = document.querySelector(".how-its-work");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  howItsWork.classList.toggle("active");
+});
